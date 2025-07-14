e902f24cd6214af45d60253dd4bb9fd178c04758
Toufiq207 <lijonlijon30@gmail.com>
2025-07-14 21:11:48 +0600

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Lizon from '../layout/Lizon'
 import Banner from '../layout/Banner'
-import Navber from '../layout/Navbar'
+// import Navber from '../layout/Navbar'
 import About from '../layout/About'
 import Expreance from '../layout/Expreance'
 import Footer from '../layout/Footer'
 import Contact from '../layout/Contact'
 import Project from '../layout/Project'
+import Navber from '../layout/Navber'
 
 
 
