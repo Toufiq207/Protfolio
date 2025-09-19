ac8fd37c7393710f0cb925bd05d84aae34db24e5
ToufiqLizon <lijonlijon30@gmail.com>
2025-09-19 23:41:35 +0600

    update

--- src/component/Box.jsx
+++ src/component/Box.jsx
@@ -4,11 +4,11 @@ import Heading from './Heading'
 const Box = ({text}) => {
   return (
     <div className=' relative'>
-        <div className='w-[400px] h-[400px]  flex justify-center items-center border border-black'>
+        <div className='w-[200px]  lg:w-[400px] xl:w-[400px]  md:w-[400px] h-[200px] lg:h-[400px] xl:h-[400px] md:h-[400px]  flex justify-center items-center border border-black'>
           <Heading  text={text}/>
         
         </div>
-        <div className='w-[400px] h-[400px]  flex justify-center items-center bg-[#0707ecbf] absolute top-0 hover:bg-transparent duration-500'>
+        <div className='w-[200px]  lg:w-[400px] xl:w-[400px]  md:w-[400px] h-[200px] lg:h-[400px] xl:h-[400px] md:h-[400px]  flex justify-center items-center bg-[#0707ecbf] absolute top-0 hover:bg-transparent duration-500'>
           
         
         </div>
